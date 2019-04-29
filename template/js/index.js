7bfdeb1c2a2dd74186c568e02d69318da69424f9
Leonardo Matos <leomp120894@gmail.com>
2019-04-29 14:46:23 -0300

    chore: fix dependencies, load widgets async

--- template/js/index.js
+++ template/js/index.js
@@ -1,2 +1,11 @@
 
 'use strict'
+
+// setup dependencies
+import '@ecomplus/storefront-renderer/dist/storefront.min.js'
+import '@ecomplus/shopping-cart'
+
+// async dependencies
+import(/* webpackChunkName: "@ecomplus/widget-minicart" */ '@ecomplus/widget-minicart')
+import(/* webpackChunkName: "@ecomplus/widget-search" */ '@ecomplus/widget-search')
+import(/* webpackChunkName: "@ecomplus/widget-user" */ '@ecomplus/widget-user')

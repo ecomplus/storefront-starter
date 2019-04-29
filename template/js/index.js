2b4d3f54fd4f0ee297d2a103d896aad4b507a571
Leonardo Matos <leomp120894@gmail.com>
2019-04-29 15:37:13 -0300

    chore: update dependencies, lazy load netlify identity script

--- template/js/index.js
+++ template/js/index.js
@@ -6,6 +6,7 @@ import '@ecomplus/storefront-renderer/dist/storefront.min.js'
 import '@ecomplus/shopping-cart'
 
 // async dependencies
+// storefront widgets
 import(/* webpackChunkName: "@ecomplus/widget-minicart" */ '@ecomplus/widget-minicart')
 import(/* webpackChunkName: "@ecomplus/widget-search" */ '@ecomplus/widget-search')
 import(/* webpackChunkName: "@ecomplus/widget-user" */ '@ecomplus/widget-user')

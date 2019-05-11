9e70818bccf30176ece100d2c6f967db40fc0851
Leonardo Matos <leomp120894@gmail.com>
2019-05-11 01:52:01 -0300

    feat: setup glide options from element data

--- template/js/src/utils.js
+++ template/js/src/utils.js
@@ -40,8 +40,33 @@ EcomInit.then(() => {
   let $glides = document.getElementsByClassName('glide')
   for (let i = 0; i < $glides.length; i++) {
     let $glide = $glides[i]
-    let autoplay = parseInt($glide.dataset.autoplay, 10)
-    new Glide($glide, { autoplay }).mount().on('move', () => {
+    // setup glide options from element data
+    // https://glidejs.com/docs/options/
+    let options = {
+      type: $glide.dataset.type
+    }
+    ;[ 'autoplay', 'perView' ].forEach(opt => {
+      if ($glide.dataset.hasOwnProperty(opt)) {
+        options[opt] = parseInt($glide.dataset[opt], 10)
+      }
+    })
+
+    // per view breakpoints following Bootstrap grid
+    let grid = { Xs: 576, Sm: 768, Md: 992, Lg: 1200 }
+    options.breakpoints = {}
+    for (let maxSize in grid) {
+      if (grid.hasOwnProperty(maxSize)) {
+        let perView = $glide.dataset['perView' + maxSize]
+        if (perView) {
+          options.breakpoints[maxSize] = {
+            perView: parseInt(perView, 10)
+          }
+        }
+      }
+    }
+
+    // new slider instance
+    new Glide($glide, options).mount().on('move', () => {
       // mannualy trigger slide images load
       let $lozad = $glide.getElementsByClassName('lozad')
       for (let i = 0; i < $lozad.length; i++) {

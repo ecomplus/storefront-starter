c3d1fb10ccccdbd85db490aeee67976db149d013
Leonardo Matos <leomp120894@gmail.com>
2019-05-17 19:30:00 -0300

    feat(product): handling gallery and thumbs

--- template/js/src/pages/product.js
+++ template/js/src/pages/product.js
@@ -1,1 +1,19 @@
 'use strict'
+
+/* global Gallery, Thumbs */
+// Storefront renderer init promise
+/* global EcomInit */
+// Wait utils.js load
+/* global SetupUtils */
+
+SetupUtils.then(() => {
+  EcomInit.then(() => {
+    // handle product images gallery
+    Gallery.on('move', () => {
+      Thumbs.go('=' + Gallery.index)
+    })
+    window.moveGallery = index => {
+      Gallery.go('=' + index)
+    }
+  })
+})

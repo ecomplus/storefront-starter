37cc4bd789f7a89b72d1a78c970e2917af61e85d
Leonardo Matos <leomp120894@gmail.com>
2019-05-17 16:15:01 -0300

    fix(js): fixes for glide carousel and sliders

--- template/js/index.js
+++ template/js/index.js
@@ -5,8 +5,15 @@ import '@ecomplus/storefront-renderer/dist/storefront.min.js'
 import '@ecomplus/shopping-cart'
 
 // main components
-import './src/components/header'
+import './src/partials/header'
 
 // async load
 import('./src/utils')
 import('./src/icons')
+
+// import scripts by page
+let $main = document.getElementById('__main')
+if ($main) {
+  let { page } = $main.dataset
+  import('./src/pages/' + page)
+}

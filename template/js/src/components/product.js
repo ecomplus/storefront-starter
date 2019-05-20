beade5a0726e1bc62d8288426c055b57919b7316
Leonardo Matos <leomp120894@gmail.com>
2019-05-20 16:21:56 -0300

    feat(product): setup pictures gallery with photoswipe

--- template/js/src/components/product.js
+++ template/js/src/components/product.js
@@ -1,19 +1,60 @@
 'use strict'
 
-/* global Gallery, Thumbs */
+/* global Stage, Thumbs */
 // Storefront renderer init promise
-/* global EcomInit */
+/* global EcomInit, Ecom */
 // Wait utils.js load
 /* global SetupUtils */
 
+// responsive gallery for product pictures with PhotoSwipe
+import PhotoSwipe from 'photoswipe'
+import PhotoSwipeUI from 'photoswipe/dist/photoswipe-ui-default'
+
 SetupUtils.then(() => {
   EcomInit.then(() => {
-    // handle product images gallery
-    Gallery.on('move', () => {
-      Thumbs.go('=' + Gallery.index)
+    // handle product images stage and thumbnails
+    Stage.on('move', () => {
+      Thumbs.go('=' + Stage.index)
     })
-    window.moveGallery = index => {
-      Gallery.go('=' + index)
+    window.moveStage = index => {
+      Stage.go('=' + index)
+    }
+
+    const { currentObject } = Ecom
+    // PhotoSwipe images list
+    const psImages = []
+    if (currentObject) {
+      const { pictures, name } = currentObject
+      if (pictures && pictures.length) {
+        // setup PhotoSwipe items
+        // https://photoswipe.com/documentation/getting-started.html
+        pictures.forEach(({ zoom }) => {
+          if (zoom && zoom.size) {
+            let sizes = zoom.size.split('x')
+            // PhotoSwipe requires image dimensions to work properly
+            if (sizes.length === 2) {
+              psImages.push({
+                src: zoom.url,
+                title: name,
+                w: parseInt(sizes[0], 10),
+                h: parseInt(sizes[1], 10)
+              })
+            }
+          }
+        })
+      }
+    }
+
+    window.openGallery = index => {
+      if (psImages.length) {
+        // open full screen images gallery
+        const $pswp = document.getElementsByClassName('pswp')[0]
+        if ($pswp) {
+          // initializes and opens PhotoSwipe
+          const gallery = new PhotoSwipe($pswp, PhotoSwipeUI, psImages, { index })
+          gallery.init()
+        }
+      }
     }
   })
 })

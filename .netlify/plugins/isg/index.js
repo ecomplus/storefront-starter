8521033482d03418ac5074896db6cc88e7f8c546
Leonardo Matos <leomp120894@gmail.com>
2020-10-13 15:47:29 -0300

    feat(netlify-isg): setup netlify plugin for isg with cache and git utils
    https://github.com/ecomplus/storefront/issues/172

---
+++ .netlify/plugins/isg/index.js
@@ -0,0 +1,30 @@
+const getCompiledFiles = ({ utils }) => {
+  return utils.git.createdFiles.concat(utils.git.modifiedFiles)
+}
+
+const handleCacheList = async ({ utils, constants }, filepaths, method) => {
+  for (let i = 0; i < filepaths.length; i++) {
+    if (filepaths[i].startsWith(`${constants.PUBLISH_DIR}/`)) {
+      await utils.cache[method](filepaths[i])
+    }
+  }
+}
+
+module.exports = {
+  async onPreBuild (context) {
+    if (getCompiledFiles().includes('.isg')) {
+      const cachedFiles = await context.utils.cache.list()
+      await handleCacheList(context, cachedFiles, 'restore')
+    }
+  },
+
+  async onPostBuild (context) {
+    const compiledFiles = getCompiledFiles()
+    await handleCacheList(context, compiledFiles, 'save')
+    if (!compiledFiles.includes('.isg')) {
+      const oldCacheFiles = await context.utils.cache.list()
+        .filter(filepath => !compiledFiles.includes(filepath))
+      await handleCacheList(context, oldCacheFiles, 'remove')
+    }
+  }
+}

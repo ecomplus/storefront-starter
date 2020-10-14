c0d893c61bdde0267d279f49e2df3290c4fb628d
Leonardo Matos <leomp120894@gmail.com>
2020-10-14 04:29:39 -0300

    perf(isg-plugin): simpler cache handlers by publish dir only

--- .netlify/plugins/isg/index.js
+++ .netlify/plugins/isg/index.js
@@ -1,46 +1,24 @@
-const handleCacheList = async ({ utils, constants }, filepaths, method) => {
-  for (let i = 0; i < filepaths.length; i++) {
-    const filepath = filepaths[i]
-    if (filepath.startsWith(`${constants.PUBLISH_DIR}/`)) {
-      console.log(`ISG ${method} ${filepath}`)
-      await utils.cache[method](filepath)
-    } else {
-      console.log(`ISG ignore ${filepath}`)
-    }
-  }
-}
-
 module.exports = {
-  async onPostBuild (context) {
-    const { utils, constants } = context
-    const compiledFiles = utils.git.createdFiles.concat(utils.git.modifiedFiles)
-    const isIsg = compiledFiles.includes('.isg')
+  async onPostBuild ({ utils, constants }) {
+    const { PUBLISH_DIR } = constants
+    let isIsg
 
-    const absoluteCachedFiles = await context.utils.cache.list()
-    const testFile = `/${constants.PUBLISH_DIR}/index.html`
-    const absolutePath = absoluteCachedFiles
-      .find(filepath => filepath.endsWith(testFile))
-    let cachedFiles
-    if (absolutePath) {
-      const basePath = absolutePath.replace(testFile, '/')
-      cachedFiles = absoluteCachedFiles.map(filepath => filepath.replace(basePath, ''))
-    } else {
-      cachedFiles = absoluteCachedFiles
+    try {
+      const { createdFiles, modifiedFiles } = utils.git
+      isIsg = createdFiles.includes('.isg') || modifiedFiles.includes('.isg')
+    } catch (err) {
+      console.error(err)
+      isIsg = false
     }
 
     if (isIsg) {
-      console.log('>> Restoring cache')
-      await handleCacheList(context, cachedFiles, 'restore')
-    }
-
-    console.log('>> Saving cache')
-    await handleCacheList(context, compiledFiles, 'save')
-
-    if (!isIsg) {
-      console.log('>> Removing old cache')
-      const oldCachedFiles = cachedFiles
-        .filter(filepath => utils.git.deletedFiles.includes(filepath))
-      await handleCacheList(context, oldCachedFiles, 'remove')
+      console.log('>> Restoring ISG cache')
+      await utils.cache.restore(PUBLISH_DIR)
+    } else {
+      console.log('>> Removing ISG old cache')
+      await utils.cache.remove(PUBLISH_DIR)
+      console.log('>> Saving ISG cache')
+      await utils.cache.save(PUBLISH_DIR)
     }
   }
 }

9855e0140a76ab92e8017b1e16bc49080e0111aa
Leonardo Matos <leomp120894@gmail.com>
2020-10-14 19:34:26 -0300

    fix(ssr): specific package for ssr functions with defined dependent files (assets)

---
+++ .netlify/functions/ssr/index.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+exports.handler = (ev, context, callback) => {
+  if (/\.(js|css|ico|png|gif|jpg|jpeg|webp|svg)$/.test(ev.path)) {
+    return callback(null, {
+      statusCode: 404,
+      'Cache-Control': 'public, max-age=60'
+    })
+  }
+
+  let statusCode = 200
+  const headers = {}
+
+  const req = {
+    url: ev.path.charAt(0) === '/' ? ev.path : `/${ev.path}`
+  }
+
+  const res = {
+    set (header, value) {
+      headers[header] = value
+      return res
+    },
+    status (status) {
+      statusCode = status
+      return res
+    },
+    end () {
+      callback(null, { statusCode, headers })
+      return res
+    },
+    send (body) {
+      callback(null, { statusCode, headers, body })
+      return res
+    }
+  }
+
+  process.env.STOREFRONT_BASE_DIR = __dirname
+  process.env.STOREFRONT_BUNDLES_PATH = path.join(`${__dirname}/bundles.json`)
+
+  const { ssr } = require('@ecomplus/storefront-renderer/functions/')
+  ssr(req, res)
+}

bf998a894ee7c2da08e05c2f10ea7cb9df38ccd1
Leonardo Matos <leomp120894@gmail.com>
2020-10-14 00:07:16 -0300

    chore: remove firebase assets

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions')
-
-const fns = require('@ecomplus/storefront-renderer/functions/')
-
-for (const fnName in fns) {
-  if (typeof fns[fnName] === 'function') {
-    exports[fnName] = functions.https.onRequest((req, res) => fns[fnName](req, res))
-  }
-}

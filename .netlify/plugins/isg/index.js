6ed636badede250f5b5dd1cf0f30e92b8318a1f2
Leonardo Matos <leomp120894@gmail.com>
2020-10-14 01:06:44 -0300

    chore(isg-plugin): debug cache handlers

--- .netlify/plugins/isg/index.js
+++ .netlify/plugins/isg/index.js
@@ -4,8 +4,10 @@ const getCompiledFiles = ({ utils }) => {
 
 const handleCacheList = async ({ utils, constants }, filepaths, method) => {
   for (let i = 0; i < filepaths.length; i++) {
-    if (filepaths[i].startsWith(`${constants.PUBLISH_DIR}/`)) {
-      await utils.cache[method](filepaths[i])
+    const filepath = filepaths[i]
+    if (filepath.startsWith(`${constants.PUBLISH_DIR}/`)) {
+      console.log(`Cache ${method} ${filepath}`)
+      await utils.cache[method](filepath)
     }
   }
 }
@@ -13,12 +15,14 @@ const handleCacheList = async ({ utils, constants }, filepaths, method) => {
 module.exports = {
   async onPreBuild (context) {
     if (getCompiledFiles(context).includes('.isg')) {
+      console.log('>> Restoring cache')
       const cachedFiles = await context.utils.cache.list()
       await handleCacheList(context, cachedFiles, 'restore')
     }
   },
 
   async onPostBuild (context) {
+    console.log('>> Saving cache')
     const compiledFiles = getCompiledFiles(context)
     await handleCacheList(context, compiledFiles, 'save')
     if (!compiledFiles.includes('.isg')) {

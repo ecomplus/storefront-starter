c122d47074e1d0347adb8d69aaea672bcd90ad31
Leonardo Matos <leomp120894@gmail.com>
2020-10-14 01:19:38 -0300

    perf(isg-plugin): run only post build hook

--- .netlify/plugins/isg/index.js
+++ .netlify/plugins/isg/index.js
@@ -1,32 +1,28 @@
-const getCompiledFiles = ({ utils }) => {
-  return utils.git.createdFiles.concat(utils.git.modifiedFiles)
-}
-
 const handleCacheList = async ({ utils, constants }, filepaths, method) => {
   for (let i = 0; i < filepaths.length; i++) {
     const filepath = filepaths[i]
     if (filepath.startsWith(`${constants.PUBLISH_DIR}/`)) {
-      console.log(`Cache ${method} ${filepath}`)
+      console.log(`ISG ${method} ${filepath}`)
       await utils.cache[method](filepath)
+    } else {
+      console.log(`ISG ignore ${filepath}`)
     }
   }
 }
 
 module.exports = {
-  async onPreBuild (context) {
-    if (getCompiledFiles(context).includes('.isg')) {
+  async onPostBuild (context) {
+    const { utils } = context
+    const compiledFiles = utils.git.createdFiles.concat(utils.git.modifiedFiles)
+    const cachedFiles = await context.utils.cache.list()
+    const isIsg = compiledFiles.includes('.isg')
+    if (isIsg) {
       console.log('>> Restoring cache')
-      const cachedFiles = await context.utils.cache.list()
       await handleCacheList(context, cachedFiles, 'restore')
     }
-  },
-
-  async onPostBuild (context) {
     console.log('>> Saving cache')
-    const compiledFiles = getCompiledFiles(context)
     await handleCacheList(context, compiledFiles, 'save')
-    if (!compiledFiles.includes('.isg')) {
-      const cachedFiles = await context.utils.cache.list()
+    if (!isIsg) {
       const oldCachedFiles = cachedFiles.filter(filepath => !compiledFiles.includes(filepath))
       await handleCacheList(context, oldCachedFiles, 'remove')
     }
